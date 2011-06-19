c4e9aee422aaee4f3d9a3995b87d5c9a1cd7d475
zibalas <zibalas@gmail.com>
2011-06-19 12:28:08 +0300

    adding documentation

--- ListExtensions.cs
+++ ListExtensions.cs
@@ -8,35 +8,56 @@ namespace ZExtensions
 {
     public static class ListExtensions
     {
-        public static void SafeAdd(this IList list, object value)
+        /// <summary>
+        /// Adds item to the list if items is not already added
+        /// </summary>
+        /// <param name="list">Owner list</param>
+        /// <param name="item">Item to add</param>
+        public static void SafeAdd(this IList list, object item)
         {
-            if (!list.Contains(value))
+            if (!list.Contains(item))
             {
-                list.Add(value);
+                list.Add(item);
             }
         }
 
-        public static void SafeAdd<T>(this IList<T> list, T value)
+        /// <summary>
+        /// Adds item to the list if items is not already added
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Owner list</param>
+        /// <param name="item">Item to add</param>
+        public static void SafeAdd<T>(this IList<T> list, T item)
         {
-            if (!list.Contains(value))
+            if (!list.Contains(item))
             {
-                list.Add(value);
+                list.Add(item);
             }
         }
 
-        public static void SafeRemove(this IList list, object value)
+        /// <summary>
+        /// Remove item from the list, if list contains the item
+        /// </summary>
+        /// <param name="list">Owner list</param>
+        /// <param name="item">Item to remove</param>
+        public static void SafeRemove(this IList list, object item)
         {
-            if (list.Contains(value))
+            if (list.Contains(item))
             {
-                list.Remove(value);
+                list.Remove(item);
             }
         }
 
-        public static void SafeRemove<T>(this IList<T> list, T value)
+        /// <summary>
+        /// Remove item from the list, if list contains the item
+        /// </summary>
+        /// <param name="list">Owner list</param>
+        /// <param name="item">Item to remove</param>
+        public static void SafeRemove<T>(this IList<T> list, T item)
         {
-            if (list.Contains(value))
+            if (list.Contains(item))
             {
-                list.Remove(value);
+                list.Remove(item);
             }
         }
     }
